ffd2a00656625f24ffccff71179e2b5a7fb01c71
aylancOnur <aylanc.onur@gmail.com>
2022-03-18 16:50:28 +0300

    redis pubsub and .env added

--- comment-challenge/index.js
+++ comment-challenge/index.js
@@ -1,8 +1,14 @@
-const { ApolloServer, gql } = require("apollo-server");
+const {
+  GraphQLServer,
+  PubSub,
+  withFilter,
+} = require("graphql-yoga");
+// const pubsub = require("./pubsub.js");
+// const { ApolloServer, gql } = require("apollo-server");
 const { users, posts, comments } = require("./data.js");
 const { nanoid } = require("nanoid");
 
-const typeDefs = gql`
+const typeDefs = `
   # User
   type User {
     id: ID!
@@ -97,6 +103,22 @@ const typeDefs = gql`
     deleteComment(id: ID!): Comment!
     deleteAllComments: DeleteAllOutput!
   }
+
+  type Subscription {
+    # User
+    userCreated: User!
+    userUpdated: User!
+    userDeleted: User!
+    # Post
+    postCreated(user_id: ID): Post!
+    postUpdated: Post!
+    postDeleted: Post!
+    postCount: Int!
+    # Comment
+    commentCreated(post_id: ID): Comment!
+    commentUpdated: Comment!
+    commentDeleted: Comment!
+  }
 `;
 
 const resolvers = {
@@ -127,12 +149,13 @@ const resolvers = {
   },
   Mutation: {
     // User
-    createUser: (parent, { data: { fullName } }) => {
-      const user = { id: nanoid(), fullName: fullName };
+    createUser: (parent, { data: { fullName, age } }, { pubsub }) => {
+      const user = { id: nanoid(), fullName: fullName, age };
       users.push(user);
+      pubsub.publish("userCreated", { userCreated: user });
       return user;
     },
-    updateUser: (parent, { id, data }) => {
+    updateUser: (parent, { id, data }, { pubsub }) => {
       const user_index = users.findIndex((user) => user.id === id);
       if (user_index === -1) {
         throw new Error("User not found!");
@@ -142,9 +165,10 @@ const resolvers = {
         ...users[user_index],
         ...data,
       });
+      pubsub.publish("userUpdated", { userUpdated: updatedUser });
       return updatedUser;
     },
-    deleteUser: (parent, { id }) => {
+    deleteUser: (parent, { id }, { pubsub }) => {
       const user_index = users.findIndex((user) => user.id === id);
       if (user_index === -1) {
         throw new Error("User not found!");
@@ -153,6 +177,7 @@ const resolvers = {
       // birinci paremetre hangi indexten sonra
       // ikinci paremetre kaçtane kaldıracağımız
       users.splice(user_index, 1);
+      pubsub.publish("userDeleted", { userDeleted: deleted_user });
       return deleted_user;
     },
     deleteAllUsers: (parent, args) => {
@@ -165,12 +190,14 @@ const resolvers = {
     },
 
     // Post
-    createPost: (parent, { data: { title, user_id } }) => {
+    createPost: (parent, { data: { title, user_id } }, { pubsub }) => {
       const post = { id: nanoid(), title, user_id };
       posts.push(post);
+      pubsub.publish("postCreated", { postCreated: post });
+      pubsub.publish("postCount", { postCount: posts.length });
       return post;
     },
-    updatePost: (parent, { id, data }) => {
+    updatePost: (parent, { id, data }, { pubsub }) => {
       const post_index = posts.findIndex((post) => post.id === id);
       if (post_index === -1) {
         throw new Error("Post not found!");
@@ -179,32 +206,36 @@ const resolvers = {
         ...posts[post_index],
         ...data,
       });
+      pubsub.publish("postUpdated", { postUpdated: updatedPost });
       return updatedPost;
     },
-    deletePost: (parent, { id }) => {
+    deletePost: (parent, { id }, { pubsub }) => {
       const post_index = posts.findIndex((post) => post.id === id);
       if (post_index === -1) {
         throw new Error("Post not found!");
       }
       const deleted_post = posts[post_index];
       posts.splice(post_index, 1);
+      pubsub.publish("postDeleted", { postDeleted: deleted_post });
+      pubsub.publish("postCount", { postCount: posts.length });
       return deleted_post;
     },
-    deleteAllPosts: (parent, args) => {
+    deleteAllPosts: (parent, args, { pubsub }) => {
       const length = posts.length;
       posts.splice(0, length);
-
+      pubsub.publish("postCount", { postCount: posts.length });
       return {
         count: length,
       };
     },
     // Comment
-    createComment: (parent, { data }) => {
+    createComment: (parent, { data }, { pubsub }) => {
       const comment = { id: nanoid(), ...data };
       comments.push(comment);
+      pubsub.publish("commentCreated", { commentCreated: comment });
       return comment;
     },
-    updateComment: (parent, { id, data }) => {
+    updateComment: (parent, { id, data }, { pubsub }) => {
       const comment_index = comments.findIndex((comment) => comment.id === id);
       if (comment_index === -1) {
         throw new Error("Comment not found!");
@@ -213,15 +244,17 @@ const resolvers = {
         ...comments[comment_index],
         ...data,
       });
+      pubsub.publish("commentUpdated", { commentUpdated: updatedComment });
       return updatedComment;
     },
-    deleteComment: (parent, { id }) => {
+    deleteComment: (parent, { id }, { pubsub }) => {
       const comment_index = comments.findIndex((comment) => comment.id === id);
       if (comment_index === -1) {
         throw new Error("Comment not found!");
       }
       const deleted_comment = comments[comment_index];
       comments.splice(comment_index, 1);
+      pubsub.publish("commentDeleted", { commentDeleted: deleted_comment });
       return deleted_comment;
     },
     deleteAllComments: (parent, args) => {
@@ -233,10 +266,78 @@ const resolvers = {
       };
     },
   },
+  Subscription: {
+    // User
+    userCreated: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("userCreated"),
+    },
+    userUpdated: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("userUpdated"),
+    },
+    userDeleted: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("userDeleted"),
+    },
+    // Post
+    postCreated: {
+      subscribe: withFilter(
+        (parent, args, { pubsub }) => pubsub.asyncIterator("postCreated"),
+        (payload, variables) => {
+          console.log("payload =>", payload, "variables =>", variables);
+          return variables.user_id
+            ? payload.postCreated.user_id === variables.user_id
+            : true;
+        }
+      ),
+    },
+    postUpdated: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("postUpdated"),
+    },
+    postDeleted: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("postDeleted"),
+    },
+    postCount: {
+      subscribe: (parent, args, { pubsub }) => {
+        setTimeout(() => {
+          pubsub.publish("postCount", { postCount: posts.length });
+        }, 1000);
+
+        return pubsub.asyncIterator("postCount");
+      },
+    },
+    // Comment
+    commentCreated: {
+      subscribe: withFilter(
+        (parent, args, { pubsub }) => pubsub.asyncIterator("commentCreated"),
+        (payload, variables) => {
+          console.log("payload =>", payload, "variables =>", variables);
+          return variables.post_id
+            ? payload.commentCreated.post_id === variables.post_id
+            : true;
+        }
+      ),
+    },
+    commentUpdated: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("commentUpdated"),
+    },
+    commentDeleted: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("commentDeleted"),
+    },
+  },
 };
 
 // 1 => paremetre oluşturduğumuz type tanımları
 // 2 => bu tiplere cevap dönecek olan resolverlar
-const server = new ApolloServer({ typeDefs, resolvers });
+// const server = new ApolloServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
 
-server.listen().then(({ url }) => console.log(`Apollo Server is up at ${url}`));
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+server.start(() => console.log("Server is running on localhost:4000"));
+
+// server.listen().then(({ url }) => console.log(`Apollo Server is up at ${url}`));
